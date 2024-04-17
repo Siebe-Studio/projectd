4a6f87e2642a91cb5ff5ae8143e4945d63ac3875
Lombex <LombexTV@gmail.com>
2024-04-17 19:33:05 +0200

    Added Remove and edit vehicle options

--- src/app/dashboard/voertuigen/page.tsx
+++ src/app/dashboard/voertuigen/page.tsx
@@ -1,11 +1,15 @@
 
 import VehicleTable from "~/components/voertuigen/VehicleTables";
 import { CreateVehicle } from "~/components/voertuigen/CreateVehicle";
+import { RemoveVehicle } from "~/components/voertuigen/RemoveVehicle";
+import { EditVehicle } from "~/components/voertuigen/EditVehicle";
 export default async function Voertuigen() {
     return (
         <div className="w-full flex-col flex gap-4 px-3">
             <div className="w-full flex gap-2">
             <CreateVehicle/>
+            <RemoveVehicle/>
+            <EditVehicle/>
             </div>
             <VehicleTable/>
         </div>
